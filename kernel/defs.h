f88fc5026da0d783515260cb3d31d7329d7e6d53
LeRobbii <113675407+LeRobbii@users.noreply.github.com>
2026-03-28 07:26:36 +0100

    Added definition of custom system calls

--- kernel/defs.h
+++ kernel/defs.h
@@ -101,6 +101,12 @@ void            yield(void);
 int             either_copyout(int user_dst, uint64 dst, void *src, uint64 len);
 int             either_copyin(void *dst, int user_src, uint64 src, uint64 len);
 void            procdump(void);
+//custom calls in proc.c
+int             getnice(int pid);
+int             setnice(int pid, int value);
+void            ps(int pid);
+int             meminfo(int pid);
+int             waitpid(int pid);
 
 // swtch.S
 void            swtch(struct context*, struct context*);
