d4c45c01ab7c5db4f0065622b8660359a861454c
LeRobbii <113675407+LeRobbii@users.noreply.github.com>
2026-03-28 08:43:38 +0100

    Fix type error on custom calls

--- kernel/defs.h
+++ kernel/defs.h
@@ -102,11 +102,11 @@ int             either_copyout(int user_dst, uint64 dst, void *src, uint64 len);
 int             either_copyin(void *dst, int user_src, uint64 src, uint64 len);
 void            procdump(void);
 //custom calls in proc.c
-int             getnice(int pid);
-int             setnice(int pid, int value);
+uint64          getnice(int pid);
+uint64          setnice(int pid, int value);
 void            ps(int pid);
-int             meminfo(int pid);
-int             waitpid(int pid);
+uint64          meminfo(int pid);
+uint64          waitpid(int pid);
 
 // swtch.S
 void            swtch(struct context*, struct context*);
