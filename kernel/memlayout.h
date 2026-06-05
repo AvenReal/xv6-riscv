dbfb674554ad305c5ba759f249f2eddb7d93be98
LeRobbii <le.robbi2@gmail.com>
2026-06-05 08:10:13 +0200

    finishing adding constants

--- kernel/memlayout.h
+++ kernel/memlayout.h
@@ -37,7 +37,10 @@
 // for use by the kernel and user pages
 // from physical address 0x80000000 to PHYSTOP.
 #define KERNBASE 0x80000000L
-#define PHYSTOP (KERNBASE + 128*1024*1024)
+//#define PHYSTOP (KERNBASE + 128*1024*1024)
+#define SWAPBASE 2000
+#define SWAPMAX  28000
+#define PHYSTOP  (KERNBASE + 4*1024*1024 + 512*1024)  // 4.5 MiB; tune as needed
 
 // map the trampoline page to the highest address,
 // in both user and kernel space.
